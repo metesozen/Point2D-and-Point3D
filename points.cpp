bba116b314539ec44eae358409d6036261a81798
metesozen <56272425+metesozen@users.noreply.github.com>
2021-03-20 15:18:39 +0300

    Update points.cpp

--- points.cpp
+++ points.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 class Point2D{
     public:
     int x;
@@ -28,3 +31,26 @@ class Point3D : public Point2D{
         cout<<"3D distance = "<<ceil(d)<<endl;
     }
 };
+int main() {
+    int x1;
+    int y1;
+    int z1;
+    int x2;
+    int y2;
+    int z2;
+    
+    cin >> x1 >> y1 >> z1;
+    cin >> x2 >> y2 >> z2; 
+    
+    Point3D p1(x1, y1, z1);
+    Point3D p2(x2, y2, z2);
+    double d2 = p1.dist2D(p2);
+    double d3 = p1.dist3D(p2);
+    //The code below uses runtime polymorphism to call the overridden printDistance method:
+    Point2D p(0, 0);
+    p.printDistance(d2);
+    p = p1;
+    p1.printDistance(d3);
+    
+    return 0;
+}
